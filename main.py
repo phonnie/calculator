b93f53df9dddb9dabd2a22c4af895ce122b90be8
D416-18\user <psubbb@gmail.com>
2023-03-21 14:30:08 +0900

    라이브러리 설치

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
-                             QMessageBox, QPlainTextEdit, QHBoxLayout)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 
